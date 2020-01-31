0e0ed0bbef9ce91079f62e6d7df3b06ce35e9f87
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-01-31 10:53:44 +0700

    Created project.

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {styles} from "./styles";
+import {Love} from "./Love";
+
+export default function App() {
+    return (
+        <Love style={styles.container}/>
+    );
+}
+
---
+++ Love.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+export function Love({style}) {
+    return (
+        <View style={style}>
+            <Text>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dicta facilis id iusto maxime, molestias numquam provident saepe! Adipisci architecto fugiat iste, labore libero nisi ut. Accusamus ipsam ut voluptas!
+            </Text>
+        </View>
+    );
+}
